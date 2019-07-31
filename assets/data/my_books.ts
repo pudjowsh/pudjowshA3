849ce438eb94424372265d35ee83a3863c908bb4
pudjowsh <53275507+pudjowsh@users.noreply.github.com>
2019-07-30 22:47:49 -0400

    Add files via upload

---
+++ assets/data/my_books.ts
@@ -0,0 +1,33 @@
+import { book_info } from 'src/app/book_info'
+
+export const MYBOOKS: book_info[] = 
+[
+    {
+        book_name: "Mistborn: The Final Empire",
+        author: "Brandon Sanderson",
+        book_genre: "Epic Fantasy",
+        year_published: "2006",
+        picture: 'mistborn.jpg'
+    },
+    {
+        book_name: "Neuromancer",
+        author: "William Gibson",
+        book_genre: "Science Fiction",
+        year_published: "1984",
+        picture: 'neuromancer.jpg'
+    },
+    {
+        book_name: "Simulacra et Simulation",
+        author: "Jean Baudrillard",
+        book_genre: "Philosophy",
+        year_published: "1981",
+        picture: 'simulacra.jpg'
+    },
+    {
+        book_name: "12 Rules for Life",
+        author: "Jordan Peterson",
+        book_genre: "Self-help Book",
+        year_published: "2018",
+        picture: '12_rules.jpg'
+    }
+]
